dba3ff90f2af8f83b444831f664eb9a81f2603a8
Simba Ho <cphotor@icloud.com>
2026-04-23 10:58:52 +0800

    fix: Export CommandHistory from commands package

--- cogist/application/commands/__init__.py
+++ cogist/application/commands/__init__.py
@@ -2,7 +2,8 @@
 
 from cogist.application.commands.add_node_command import AddNodeCommand
 from cogist.application.commands.command import Command
+from cogist.application.commands.command_history import CommandHistory
 from cogist.application.commands.delete_node_command import DeleteNodeCommand
 from cogist.application.commands.edit_text_command import EditTextCommand
 
-__all__ = ["Command", "AddNodeCommand", "DeleteNodeCommand", "EditTextCommand"]
+__all__ = ["Command", "AddNodeCommand", "DeleteNodeCommand", "EditTextCommand", "CommandHistory"]
